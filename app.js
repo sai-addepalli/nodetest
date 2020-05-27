4baac909930b0824f7c14667ad5967e1e6ba9440
Sai Addepalli <saddepal@gmail.com>
2020-05-26 22:08:25 -0700

    got one sound file to play

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
 var http = require("http");
 var exp = require("express");
 var app = exp();
+var Sound = require('node-aplay');
 
-app.use(exp.static('static'));
 
 app.get('/', function(req, res) {
 	console.log("received request " + req);
-	res.sendFile("static/index.html");
+	res.sendFile("index.html", {root: "./static"});
 });
 
+app.get('/second', function(req, res) {
+	console.log("received request for second page");
+	res.sendFile("second.html", {root: "./static"});
+});
+
+
+app.get('/Reaper16', function(req, res) {
+	res.sendFile("Reaper16.wav", {root: "./resources"});
+});
 
 var server = app.listen(8080, function() {
 	console.log("Server listening");
