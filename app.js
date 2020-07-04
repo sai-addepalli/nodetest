b39430515bbfb568eefd96cea3a757c61e01dcc6
Sai Addepalli <saddepal@gmail.com>
2020-07-04 14:21:47 -0700

    post parsing doesnt work

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 var http = require("http");
 var exp = require("express");
+var bodyParser = require("body-parser");
 var app = exp();
 var Sound = require('node-aplay');
 
+var commentArr = [];
+
+//app.use(bodyParser.urlencoded({extended: false}))
+var bodyParserObj = bodyParser.urlencoded({extended: false}))
 
 app.get('/', function(req, res) {
 	console.log("received request " + req);
@@ -17,6 +22,14 @@ app.get('/second', function(req, res) {
 
 app.get('/Reaper16', function(req, res) {
 	res.sendFile("Reaper16.wav", {root: "./resources"});
+});
+
+
+app.post('/submitcomment', bodyParserObj, function(req, res) {
+	const body = req.body.Body		
+	res.set('Content-Type', 'text/plain');
+	console.log(body)
+	res.send('post received ${body} ');
 });
 
 var server = app.listen(8080, function() {
